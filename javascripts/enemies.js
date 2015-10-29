9c63e51b8a35ec7020b83460f838eb687517a311
Lauren Rouse <lauren.l.rouse@gmail.com>
2015-10-28 22:34:50 -0500

    added require definitions

--- javascripts/enemies.js
+++ javascripts/enemies.js
@@ -1,20 +1,25 @@
-var Orc = function() {
-  this.health = this.health + 20;
-  this.species = "Orc";
-  this.allowedClasses = ["Warrior", "Berserker", "Shaman"];
+define(function(require) {
+  var $ = require('jquery');
 
-  this.generateClass = function() {
-    // Get a random index from the allowed classes array
-    var random = Math.round(Math.random() * (this.allowedClasses.length - 1));
 
-    // Get the string at the index
-    var randomClass = this.allowedClasses[random];
+  var Orc = function() {
+    this.health = this.health + 20;
+    this.species = "Orc";
+    this.allowedClasses = ["Warrior", "Berserker", "Shaman"];
 
-    // Composes the corresponding player class into the player object
-    this.class = new window[randomClass]();
-    return this.class;
+    this.generateClass = function() {
+      // Get a random index from the allowed classes array
+      var random = Math.round(Math.random() * (this.allowedClasses.length - 1));
+
+      // Get the string at the index
+      var randomClass = this.allowedClasses[random];
+
+      // Composes the corresponding player class into the player object
+      this.class = new window[randomClass]();
+      return this.class;
+    };
   };
-};
 
-Orc.prototype = new Monster();
+  Orc.prototype = new Monster();
 
+}); // end define
